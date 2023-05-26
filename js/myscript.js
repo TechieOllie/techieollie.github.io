1a3ffa76ef437d403c79482d3e7696ae18a0291c
Ollie <oliverwest06@outlook.com>
2023-05-26 12:26:03 +0000

    fix footer loading problem and started transfers to Sass

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.querySelector("body").classList.add("loaded");
+    document.querySelector("footer").classList.remove("hidden");
   }, 10);
 });
