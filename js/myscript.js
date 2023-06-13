5f765c3c2ad05363e449a884190f9770a7bd79e7
Ollie <oliverwest06@outlook.com>
2023-06-13 16:55:15 +0000

    sure

--- js/myscript.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.querySelector("body").classList.add("loaded");
-    document.querySelector("footer").classList.remove("hidden");
-  }, 10);
-});
