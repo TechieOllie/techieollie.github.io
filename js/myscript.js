541725e2ef9ab12e5752ba5ab42b55a3f09589fe
Ollie <oliverwest06@outlook.com>
2023-07-10 15:30:03 +0000

    fixed loading problems and advanced on homepage (premium)

--- js/myscript.js
+++ js/myscript.js
@@ -2,5 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.querySelector("body").classList.add("loaded");
     document.querySelector("footer").classList.remove("hidden");
+    document.querySelector("nav").classList.remove("hidden");
+    document.querySelector("section").classList.remove("hidden");
+    document.querySelector(".navbar").classList.remove("hidden");
   }, 10);
 });
