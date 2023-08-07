6d2a44a44d416e73428ad797be1c020913029c6f
Ollie <oliverwest06@outlook.com>
2023-08-07 16:01:05 +0000

    finished mobile navbar

--- js/myscript.js
+++ js/myscript.js
@@ -7,3 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".navbar").classList.remove("hidden");
   }, 10);
 });
+
+const hamburger = document.querySelector(".nav-mobile");
+const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  navBar.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    navBar.classList.remove("active");
+  })
+);
